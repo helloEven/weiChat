c367d6c216be67c6e78fa1343a8338251ee94a07
even <804230708@qq.com>
2016-07-28 12:38:24 +0800

    初始化

---
+++ weiChat/weiChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weiChat
+//
+//  Created by 刘杰 on 16/7/28.
+//  Copyright © 2016年 even. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weiChat/weiChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weiChat
+//
+//  Created by 刘杰 on 16/7/28.
+//  Copyright © 2016年 even. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
